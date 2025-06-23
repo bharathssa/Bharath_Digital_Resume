f81450c741a9ceb838b185bf38627de9ae2b1386
bharathssa <bharathssa16@gmail.com>
2025-06-24 00:26:57 +1200

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Import BrowserRouter
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/Bharath_Digital_Resume/">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
