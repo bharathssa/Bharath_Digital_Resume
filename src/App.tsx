023f2dcc5d33a6d3c2ba31ebfaa31bae337bcbfd
bharathssa <bharathssa16@gmail.com>
2025-06-24 01:46:10 +1200

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StarField } from "@/components/StarField"; 
 
 // Section components
 import { Hero } from "@/components/Hero";
@@ -18,6 +19,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <StarField/>
       <Navigation />
       <Toaster />
       <Sonner />
